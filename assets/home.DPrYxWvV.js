b985c780ad357aa73963865d97cf8e888c248b71
kaizhou-chen <46644340+kaizhou-chen@users.noreply.github.com>
2024-09-01 14:20:27 +0000

    Deploying to gh-pages from @ kaizhou-chen/uniapp-example@4a39aa940d92ad883ebdbf208fd376f9a4421352 🚀

---
+++ assets/home.DPrYxWvV.js
@@ -0,0 +1,1 @@
+import{h as t}from"./http.dxpK3A_g.js";const o=(o=1)=>t({method:"GET",url:"/home/banner",data:{distributionSite:o}}),e=()=>t({method:"GET",url:"/home/category/mutli"}),a=()=>t({method:"GET",url:"/home/hot/mutli"}),h=o=>t({method:"GET",url:"/home/goods/guessLike",data:o});export{e as a,a as b,h as c,o as g};
